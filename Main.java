3b981ad7d0d94ec428183e4b6e0e937efa56dd77
otaviosenna <otavio@sharehunter.com.br>
2023-09-01 18:49:19 -0300

    Primeira leva de arquivos

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        //Arquivo main pra gente trabalhar :) 
+        
+        1) Cadastro comum (idUnica, nome, dataDeCompra, listaDeAutores, tipoDeItem)
+        
+        IF
+        2) Livros (nomeEditora, anoPublicacao)
+        
+        3) CDS (genero, nomeDasMusicas)
+        
+        4) DVD (tipo, descricaoGeral) 
+        
+        5) Revistas (anoPublicacao, nomeEditora, volumePublicacao) 
+        
+        PRINT
+        6) Imprimir lista por tipo de colecionável 
+    }
+}
