3147b4c40c8654fa236730d2cdcf6f28b485651f
Killian Domingues <killian.d@edu.pucrs.br>
2023-09-08 18:54:28 -0300

    testes objeto e listas

--- Main.java
+++ Main.java
@@ -1,8 +1,18 @@
-import util.java.Scanner;
+import java.util.Scanner;
+//import java.util.*;
 
+public class Midia{
+    
+}
 public class Main(){
     public class void main(String args[]){
         Scanner scanner = new Scanner(System.in);
-        
+
+
+ //ArrayList<Integer> lista = new ArrayList<>();
+
+//Criando uma lista usando o método List:
+       //List lista_1 = new ArrayList<Integer>();
+
     }
 }
